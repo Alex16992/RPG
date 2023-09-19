c9f36335b6256731877e605240971804b7f15d93
Деев Алексей <lehad600@gmail.com>
2023-09-19 17:25:40 +0500

    Make front-end Update

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+const currentHealth = 10; // Текущее здоровье
+const maxHealth = 10; // Максимальное здоровье
+
+const healthBar = document.getElementById('healthBar');
+const healthText = document.getElementById('healthText');
+
+const fillWidth = (currentHealth / maxHealth) * 100;
+
+healthBar.style.setProperty('--fill-width', `${fillWidth}%`);
+healthText.innerText = `${currentHealth}/${maxHealth}`;
