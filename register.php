7d07c672cb7dde62cd520b561aec991af21247ee
Деев Алексей <lehad600@gmail.com>
2023-09-29 23:06:21 +0500

    Make register

--- register.php
+++ register.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,10 +17,12 @@
 <body>
 	<main class="main">
 		<div class="form">
-			<form>
-				<input name='login' class="input" type='text' placeholder='Login...'>
-				<input name='email' class="input" type='email' placeholder='Email...'>
-				<input name='password' class="input" type='password' placeholder='Password...'>
+			<div id="err" class="err">
+			</div>
+			<form id="register-form">
+				<input name='login' class="input" type='text' placeholder='Login...' autocomplete="off">
+				<input name='email' class="input" type='email' placeholder='Email...' autocomplete="off">
+				<input name='password' class="input" type='password' placeholder='Password...' autocomplete="new-password">
 				<input name="send" class="submit" type="submit" value="Register">
 			</form>
 			<p class="form__text">
@@ -26,6 +30,41 @@
 			</p>
 		</div>
 	</main>
-	<script src="assets/JS/script.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+	<script type="text/javascript">
+	window.onload = function() {
+    loginInput.value = ' ';
+    emailInput.value = ' ';
+    passwordInput.value = ' ';
+
+    setTimeout(function() {
+        loginInput.value = '';
+        emailInput.value = '';
+        passwordInput.value = '';
+    }, 100); // Adjust the timeout as needed
+};
+		$(document).ready(function() {
+			$("#register-form").submit(function(event) {
+            event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+
+            // Собираем данные из формы
+            var formData = $(this).serialize();
+
+            $.ajax({
+                type: "POST", // Метод запроса
+                url: "reg.php", // URL-адрес обработчика PHP
+                data: formData, // Данные из формы
+                success: function(response) {
+                    // Обработка успешного ответа от сервера
+                	$("#err").html(response);
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    // Обработка ошибок
+                	console.error("Ошибка при выполнении запроса: " + errorThrown);
+                }
+            });
+        });
+		});
+	</script>
 </body>
 </html>
