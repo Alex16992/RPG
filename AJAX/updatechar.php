0b10305f0ffc8f19fdbb5080ea5d2d792e383e14
Alex 1699 <lehad600@gmail.com>
2024-04-16 17:53:04 +0500

    Adding effects when equipped

--- AJAX/updatechar.php
+++ AJAX/updatechar.php
@@ -26,7 +26,7 @@ function itemChar($item_id, $userId, $link) {
     $itemId = $equippedItems[0];
     $itemLevel = $equippedItems[1];
 
-    $sqlItems = "SELECT damage, armor, crit FROM items WHERE id = '$itemId'";
+    $sqlItems = "SELECT damage, armor, crit, effect FROM items WHERE id = '$itemId'";
     $resultItems = mysqli_query($link, $sqlItems);
 
     if (!$resultItems) {
@@ -39,6 +39,7 @@ function itemChar($item_id, $userId, $link) {
     $damageItem = 0;
     $armorItem = 0;
     $critItem = 0;
+    $effectItem = 0;
 
     // Check if damage and armor are not null, then assign the values
     if ($rowItems['damage'] !== null) {
@@ -50,44 +51,56 @@ function itemChar($item_id, $userId, $link) {
     if ($rowItems['crit'] !== null) {
         $critItem = round($rowItems['crit']);
     }
+    if ($rowItems['effect'] !== null) {
+        $effectItem = $rowItems['effect'];
+    }
 
 
-    return array('damage' => $damageItem, 'armor' => $armorItem, 'crit' => $critItem);
+    return array('damage' => $damageItem, 'armor' => $armorItem, 'crit' => $critItem, 'effect' => $effectItem);
 }
 
 $damage = 3;
 $armor = 1;
 $crit = 0;
+$effect = [];
 
 $equippedStats = itemChar($itemrow['jewelry_left'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
 
 $equippedStats = itemChar($itemrow['jewelry_right'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
 
 $equippedStats = itemChar($itemrow['helmet'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
 
 $equippedStats = itemChar($itemrow['body'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
 
 $equippedStats = itemChar($itemrow['weapon_left'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
 
 $equippedStats = itemChar($itemrow['weapon_right'], $userId, $link);
 $damage += $equippedStats['damage'];
 $crit += $equippedStats['crit'];
 $armor += $equippedStats['armor'];
+$effect[] = $equippedStats['effect'];
+
+$effectSerialized = serialize($effect);
 
 $crit = round($crit);
 
@@ -97,7 +110,7 @@ $response = array(
     'crit' => $crit,
 );
 
-$query = "UPDATE users SET damage = '$damage', armor = '$armor', crit = '$crit' WHERE id = $userId";
+$query = "UPDATE users SET damage = '$damage', armor = '$armor', crit = '$crit', effect_list = '$effectSerialized' WHERE id = $userId";
 $result = mysqli_query($link, $query);
 mysqli_close($link);
 header('Content-Type: application/json');
